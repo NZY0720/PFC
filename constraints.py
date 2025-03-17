3f030b4ff8caa99a1e97d02cfb76fc2e48587952
Niko_Neo <79552564+NZY0720@users.noreply.github.com>
2025-03-17 16:06:02 +0800

    Update constraints.py

--- constraints.py
+++ constraints.py
@@ -1,16 +1,47 @@
-# constraints.py
 import torch
 
-def power_flow_constraint(V_real_all, V_imag_all, edge_index, S_real_true, S_imag_true):
-    src_nodes, tgt_nodes = edge_index[0], edge_index[1]
+def power_flow_constraint(
+    node_feats_pred,   
+    edge_index,
+    edge_attr,
+    candidate_nodes,
+    candidate_nodes_label,
+    lambda_child=1.0
+):
+    
+    src = edge_index[0]
+    tgt = edge_index[1]
 
-    V_real_src, V_imag_src = V_real_all[src_nodes], V_imag_all[src_nodes]
-    V_real_tgt, V_imag_tgt = V_real_all[tgt_nodes], V_imag_all[tgt_nodes]
+    V_real = node_feats_pred[:,0]
+    V_imag = node_feats_pred[:,1]
+    V_real_src, V_imag_src = V_real[src], V_imag[src]
+    V_real_tgt, V_imag_tgt = V_real[tgt], V_imag[tgt]
 
     S_real_pred = V_real_src * V_real_tgt + V_imag_src * V_imag_tgt
     S_imag_pred = V_imag_src * V_real_tgt - V_real_src * V_imag_tgt
 
-    loss_real = torch.mean((S_real_pred - S_real_true)**2)
-    loss_imag = torch.mean((S_imag_pred - S_imag_true)**2)
+    S_real_true = edge_attr[:,0]
+    S_imag_true = edge_attr[:,1]
 
-    return loss_real + loss_imag
+    
+    all_loss = (S_real_pred - S_real_true)**2 + (S_imag_pred - S_imag_true)**2
+   
+    candi_label_1 = set(
+        candidate_nodes[(candidate_nodes_label == 1)].tolist()
+    )
+    child_mask = []
+    for i in range(edge_index.size(1)):
+        s = src[i].item()
+        t = tgt[i].item()
+        if (s in candi_label_1) or (t in candi_label_1):
+            child_mask.append(True)
+        else:
+            child_mask.append(False)
+
+    child_mask = torch.tensor(child_mask, dtype=torch.bool, device=all_loss.device)
+
+    loss_father_father = torch.mean(all_loss[~child_mask])  
+    loss_father_child  = torch.mean(all_loss[child_mask])   
+    loss = loss_father_father + lambda_child*loss_father_child
+
+    return loss
